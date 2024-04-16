6826817ea98fbb17a0ff59c5315b3b1d0618a5d1
Jenna Jones <jljones121@gmail.com>
2024-04-16 15:23:29 +0000

    moved variable declarations to beginning

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,11 +10,11 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	size_t nodes = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	size_t nodes = 0;
 
 	if (tree->left != NULL || tree->right != NULL)
 		nodes++;
